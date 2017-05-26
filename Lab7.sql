a023119e3e00b68033e0491006789d4ca0c6497b
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-05-26 16:31:51 +0300

    Решен 3 пункт в Лаб7

--- Lab7.sql
+++ Lab7.sql
@@ -15,7 +15,7 @@
 
 ----------------------------------------------------------------
 
-2 Дать информацию о должниках группы ПС-41 с указанием фамилии студента
+2 (СДАНО) Дать информацию о должниках группы ПС-41 с указанием фамилии студента
   и названия предмета. Должниками считаются студенты, не имеющие
   оценки по предмету, который ведется в группе.
 
@@ -38,21 +38,21 @@ student
   
 ----------------------------------------------------------------
 
-3 Дать среднюю оценку студентов по каждому предмету для тех предметов,
+3 (СДАНО) Дать среднюю оценку студентов по каждому предмету для тех предметов,
   по которым занимается не менее 10 студентов.
-  
-  SELECT valuation.id_valuation, AVG(valuation.mark) FROM valuation
-  WHERE
-  valuation.id_lesson IN (
-    SELECT lesson.id_lesson FROM
-      subject
-      LEFT JOIN lesson ON subject.id_subject = lesson.id_subject
-      LEFT JOIN student ON lesson.id_group = student.id_group
-      GROUP BY subject.id_subject, lesson.id_subject HAVING COUNT(DISTINCT student.id_student) > 3
-  )
 
-  // Показать на паре
-  
+  SELECT
+  subject.name,
+  AVG(valuation.mark)
+  FROM subject
+
+  LEFT JOIN lesson ON lesson.id_subject = subject.id_subject
+  LEFT JOIN stud_group ON lesson.id_group = stud_group.id_group
+  LEFT JOIN student ON stud_group.id_group = student.id_group
+  LEFT JOIN valuation ON (valuation.id_student = student.id_student AND valuation.id_lesson = lesson.id_lesson)
+
+  GROUP BY subject.id_subject HAVING(COUNT(DISTINCT student.id_student) >= 10)
+
 ----------------------------------------------------------------
 
 4 (СДАНО) Дать оценки студентов специальности ВМ по всем проводимым предметам
@@ -76,7 +76,7 @@ student
   
 ----------------------------------------------------------------
 
-5 Всем студентам специальности ИВТ, получившим оценки меньшие 5
+5 (СДАНО) Всем студентам специальности ИВТ, получившим оценки меньшие 5
   по предмету БД до 12.05, повысить эти оценки на 1 балл.
   
   CREATE TEMPORARY TABLE tmpTable SELECT * FROM valuation;
@@ -95,5 +95,6 @@ student
       LEFT JOIN stud_group ON stud_group.id_group = lesson.id_group
       LEFT JOIN student ON stud_group.id_group = student.id_group
       LEFT JOIN tmpTable ON (tmpTable.id_student = student.id_student AND tmpTable.id_lesson = lesson.id_lesson)
+      WHERE tmpTable.post_date < '2017-05-12'
   )
 ----------------------------------------------------------------
